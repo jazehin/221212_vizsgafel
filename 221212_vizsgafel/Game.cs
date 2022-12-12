2cd65e4a2fc5827153e98b56be3a1646a1d948ee
kiss.bence2 <kiss.bence2@28-05>
2022-12-12 09:40:45 +0100

    Add project files.

---
+++ 221212_vizsgafel/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221212_vizsgafel
+{
+    internal class Game
+    {
+        private int _year;
+        private string _title;
+        private string _genre;
+        private string _publisher;
+        private string[] _platforms;
+
+        public Game(int year, string title, string genre, string publisher, string[] platforms)
+        {
+            _year = year;
+            _title = title;
+            _genre = genre;
+            _publisher = publisher;
+            _platforms = platforms;
+        }
+
+        public Game(string line)
+        {
+            string[] _ = line.Split(';');
+            _year = int.Parse(_[0]);
+            _title = _[1];
+            _genre = _[2];
+            _publisher = _[3];
+            _platforms = _[4].Split(", ");
+        }
+
+        public int Year { get => _year; }
+        public string Title { get => _title; }
+        public string Genre { get => _genre; }
+        public string Publisher { get => _publisher; }
+        public string[] Platforms { get => _platforms; }
+    }
+}
---
+++ 221212_vizsgafel/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace _221212_vizsgafel
+{
+    internal class Program
+    {
+        static List<Game> games = new List<Game>();
+        static void Main()
+        {
+            Readfile();
+            F1();
+            F2();
+            F3();
+        }
+
+        private static void F3()
+        {
+            string genre = "First-person shooter";
+            List<Game> gamesOfGenre = games.Where(game => game.Genre == genre).ToList();
+            
+            Console.WriteLine($"f3: összesen {gamesOfGenre.Count} FPS került fel a listára.");
+            Console.WriteLine($"\tpéldául a(z): {gamesOfGenre[new Random().Next(gamesOfGenre.Count)].Title}");
+
+        }
+
+        private static void F2()
+        {
+            Console.WriteLine("f2: ezekben az években került több, mint 10 cím a listára:");
+
+            /*
+            Dictionary<int, int> gamesPerYear = new();
+            foreach (Game game in games)
+            {
+                if (gamesPerYear.ContainsKey(game.Year)) gamesPerYear[game.Year]++;
+                else gamesPerYear.Add(game.Year, 1);
+            }
+            */
+
+            var manyGamesInAYear = games
+                .GroupBy(game => game.Year)
+                .OrderByDescending(x => x.Count())
+                .Where(x => x.Count() > 10)
+                .ToDictionary(x => x.Key, x => x.Count());
+
+            foreach (var group in manyGamesInAYear)
+            {
+                Console.WriteLine($"\t{group.Key}: {group.Value}db");
+            }
+        }
+
+        private static void F1()
+        {
+            Console.WriteLine($"f1: összesen {games.Count} játék szerepel a listában!");
+        }
+
+        private static void Readfile()
+        {
+            using StreamReader sr = new(@"..\\..\\..\\..\\res\bestgames.csv");
+            while (!sr.EndOfStream) games.Add(new(sr.ReadLine()));
+        }
+    }
+}
